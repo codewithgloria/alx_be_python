f4f4581f21ae3cd3c024e241cc438368538e9128
Gloria <abinezagloria@gmail.com>
2025-05-25 23:09:28 +0300

    task 4r

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+# asking user for their current age
+current_age = int(input("How old are you? "))
+
+# assuming current year is 2023
+future_year = 2050
+current_year = 2023
+years_difference = future_year - current_year
+
+future_age = current_age + years_difference
+
+print(f"In {future_year}, you will be {future_age} years old.")
